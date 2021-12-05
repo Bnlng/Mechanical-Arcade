16f482b82a2f985edfc5197b183afb68f1714248
Bnlng <88386307+Bnlng@users.noreply.github.com>
2021-12-05 17:49:20 +0100

    Update Arduino Code ohne Erklärungen.ino

--- Arduino Code ohne Erklärungen.ino
+++ Arduino Code ohne Erklärungen.ino
@@ -7,21 +7,21 @@ const int negXpwmPin = 6;
 const int posYpwmPin = 9;
 const int negYpwmPin = 11;
 
-const int buttonBorderLeftPin = 2;
-const int buttonBorderRightPin = 4;
-const int buttonBorderTopPin = 7;
-const int buttonBorderBottomPin = 8;
+const int buttonLeftPin = 2;
+const int buttonRightPin = 4;
+const int buttonTopPin = 7;
+const int buttonBottomPin = 8;
 
 //Joystick callibration
 int joystickXMaxValue = 955;
 int joystickXMinValue = 210;
-int joystickXCenterValue = 516;
-int joystickXCenterTollerance = 6;
+int joystickXCenterValue = 511;
+int joystickXCenterTollerance = 20;
 
 int joystickYMaxValue = 890;
 int joystickYMinValue = 155;
-int joystickYCenterValue = 522;
-int joystickYCenterTollerance = 12;
+int joystickYCenterValue = 512;
+int joystickYCenterTollerance = 20;
 
 
 int sensorValueX = 0;
@@ -30,25 +30,35 @@ int sensorValueY = 0;
 int outputValueX = 0;
 int outputValueY = 0;
 
+int buttonLeftState = 0;
+int buttonRightState = 0;
+int buttonTopState = 0;
+int buttonBottomState = 0;
+
 void setup() {
     Serial.begin(9600);
 
-    pinMode(buttonBorderLeftPin, INPUT);
-    pinMode(buttonBorderRightPin, INPUT);
-    pinMode(buttonBorderTopPin, INPUT);
-    pinMode(buttonBorderBottomPin, INPUT);
+    pinMode(buttonLeftPin, INPUT);
+    pinMode(buttonRightPin, INPUT);
+    pinMode(buttonTopPin, INPUT);
+    pinMode(buttonBottomPin, INPUT);
 }
 
 void loop() {
+    buttonLeftState = digitalRead(buttonLeftPin);
+    buttonRightState = digitalRead(buttonRightPin);
+    buttonTopState = digitalRead(buttonTopPin);
+    buttonBottomState = digitalRead(buttonBottomPin);
+
     //X-movement
     sensorValueX = analogRead(joystickXInputPin);
 
-    if (sensorValueX > (joystickXCenterValue + joystickXCenterTollerance) && digitalRead(buttonBorderLeftPin) == LOW) {
+    if (sensorValueX > (joystickXCenterValue + joystickXCenterTollerance) && buttonLeftState == LOW) {
         outputValueX = map(sensorValueX, joystickXCenterValue + joystickXCenterTollerance, joystickXMaxValue, 0, 255); 
         analogWrite(negXpwmPin, 0);
         analogWrite(posXpwmPin, outputValueX);
     }
-    else if (sensorValueX < (joystickXCenterValue - joystickXCenterTollerance) && digitalRead(buttonBorderRightPin) == LOW) {
+    else if (sensorValueX < (joystickXCenterValue - joystickXCenterTollerance) && buttonRightState == LOW) {
         outputValueX = map(sensorValueX, joystickXCenterValue - joystickXCenterTollerance, joystickYMinValue, 0, 255);
         analogWrite(posXpwmPin, 0);
         analogWrite(negXpwmPin, outputValueX);
@@ -62,12 +72,12 @@ void loop() {
     //Y-movement
     sensorValueY = analogRead(joystickYInputPin);
 
-    if (sensorValueY > (joystickYCenterValue + joystickYCenterTollerance) && digitalRead(buttonBorderTopPin) == LOW) {
+    if (sensorValueY > (joystickYCenterValue + joystickYCenterTollerance) && buttonTopState == LOW) {
         outputValueY = map(sensorValueY, joystickYCenterValue + joystickYCenterTollerance, joystickYMaxValue, 0, 255);
         analogWrite(negYpwmPin, 0);
         analogWrite(posYpwmPin, outputValueY);
     }
-    else if (sensorValueY < (joystickYCenterValue - joystickYCenterTollerance) && digitalRead(buttonBorderBottomPin) == LOW) {
+    else if (sensorValueY < (joystickYCenterValue - joystickYCenterTollerance) && buttonBottomState == LOW) {
         outputValueY = map(sensorValueY, joystickYCenterValue - joystickYCenterTollerance, joystickYMinValue, 0, 255);
         analogWrite(posYpwmPin, 0);
         analogWrite(negYpwmPin, outputValueY);
